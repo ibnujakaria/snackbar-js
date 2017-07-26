f44c761b0cd4dec9605e8aa4153d2d32e2f64e95
Nurul Huda <hudasanca@gmail.com>
2017-07-26 21:25:39 +0700

    add clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 var path = require('path')
-
+var CleanWebPackPlugin = require('clean-webpack-plugin')
 module.exports = {
   entry: './src/snackbar.js',
+  plugins: [
+    new CleanWebPackPlugin(['dist'])
+  ],
   output: {
     filename: 'snackbar.js',
     path: path.resolve(__dirname, 'dist')
