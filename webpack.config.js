bc836489f0ee3361f4fff28a80ec63e09629cefe
Nurul Huda <hudasanca@gmail.com>
2017-07-27 05:42:21 +0700

    add webpack-dev-server, add html-generator, and index.html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 var CleanWebPackPlugin = require('clean-webpack-plugin')
+
 module.exports = {
   entry: './src/snackbar.js',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
-    new CleanWebPackPlugin(['dist'])
+    new CleanWebPackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Snackbar in Action',
+      template: 'index.html'
+    })
   ],
   output: {
     filename: 'snackbar.js',
