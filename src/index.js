fffe51bc94a4c8f827246afcb323ad4fe6906422
Nurul Huda <hudasanca@gmail.com>
2017-07-27 12:46:46 +0700

    now, snacbar left-styled working

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import Snackbar from './snackbar'
 
 let middleButton = document.getElementById('middle-button')
+let leftButton = document.getElementById('left-button')
 
 middleButton.addEventListener('click', onMiddleButtonClicked)
+leftButton.addEventListener('click', onLeftButtonClicked)
 
 function onMiddleButtonClicked () {
   Snackbar
     .create()
     .setStyle('center')
     .setText('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,')
+    .show()
+}
+
+function onLeftButtonClicked () {
+  Snackbar
+    .create()
+    .setStyle('left')
+    .setText('Left snackbar :)')
     .show()
 }
 
